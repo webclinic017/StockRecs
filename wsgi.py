97ebea4a2c9782614b37acdc4d5297211dbf65b4
Shashank Vemuri <49424748+Shashankv3@users.noreply.github.com>
2020-06-13 00:10:08 -0400

    Initial Commit

---
+++ wsgi.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+import requests
+from app.parseTradingView import get_signal
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def func():
+    ticker = request.args['ticker']
+    interval = request.args['interval']
+    return get_signal(ticker, interval)
+
+if __name__ == "__main__":
+    app.run()
